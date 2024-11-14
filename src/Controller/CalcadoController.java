87308a8b3a89241d957c0b144536d0bfe05f780f
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-14 09:57:37 -0300

    :sparkles: add CalcadoController with methods for managing shoe inventory

---
+++ src/Controller/CalcadoController.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import model.Calcado;
+import repository.CalcadoRepository;
+
+public class CalcadoController implements CalcadoRepository {
+
+    @Override
+    public void adicionarCalcado(Calcado calcado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarCalcado'");
+    }
+
+    @Override
+    public void removerCalcado(Calcado calcado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removerCalcado'");
+    }
+
+    @Override
+    public void mostrarCalcados() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarCalcados'");
+    }
+
+    @Override
+    public void mostrarCalcadosPorMarca(String marca) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarCalcadosPorMarca'");
+    }
+
+    @Override
+    public void mostrarCalcadosPorModelo(String modelo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarCalcadosPorModelo'");
+    }
+
+    @Override
+    public void mostrarCalcadosPorTamanho(int tamanho) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarCalcadosPorTamanho'");
+    }
+
+}
